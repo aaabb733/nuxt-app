0739f5f45aadebdaf24d00512c48a0e07c4ffe7d
a <a>
2021-08-28 03:39:41 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+  <div v-for="user in $store.state.users" :key="user.id">
+    <NuxtLink :to="`list/${user.name}`">{{user.name}}</NuxtLink>
+    <span>{{user.desc}}</span>
+  </div>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ store/index.js
@@ -0,0 +1,14 @@
+export const state = () => ({
+    users: [
+        {
+            id: 0,
+            name: "ayumin",
+            desc: "Pokemon streamer"
+        },
+        {
+            id: 1,
+            name: "katou junichi",
+            desc: "game streamer"
+        }
+    ]
+})
