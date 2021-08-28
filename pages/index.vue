99f0db67265a99f44af15e1f1494f1a8052baef3
a <a>
2021-08-28 22:46:14 +0900

    timezone

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
 <div>
   <ul>
     <li v-for="i in [1,2,3,4,5]" :key="i">
-      <h5>{{ new Date(Date.now() - (i - 1) * 3600000 * 24).toLocaleDateString() }}</h5>
+      <h5>{{ new Date(Date.now() - (i - 1) * 3600000 * 24).toLocaleDateString("ja-JP", {timeZone: "Asia/Tokyo"}) }}</h5>
         <ol>
           <li v-for="user in $store.state.users" :key="user.id">
             <NuxtLink :to="`list/${user.name}`">{{user.name}}</NuxtLink>
