5a36165514b6e652aa47619bd838878953346105
a <a>
2021-08-28 17:54:18 +0900

    a

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,22 @@
 <template>
-    <div>
-        <NuxtLink to="/">Home</NuxtLink>
-        <Nuxt />
+    <div class="content">
+        <div class="header">
+            <NuxtLink to="/" class="header">Home</NuxtLink>
+        </div>
+        <Nuxt class="main" />
     </div>
 </template>
+
+<style scoped>
+.content {
+    margin: 20px;
+}
+/* .header {
+    background-color: #f3f3f3;
+} */
+.main {
+    margin-top: 20px;
+    padding: 1px 40px;
+    background-color: #f3f3f3;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,22 @@
 <template>
 <div>
-  <div v-for="user in $store.state.users" :key="user.id">
-    <NuxtLink :to="`list/${user.name}`">{{user.name}}</NuxtLink>
-    <span>{{user.desc}}</span>
-  </div>
+  <h4>
+    {{new Date().toLocaleDateString()}}
+  </h4>
+  <ol>
+    <li v-for="user in $store.state.users" :key="user.id">
+      <NuxtLink :to="`list/${user.name}`">{{user.name}}</NuxtLink>
+      <span>- {{user.desc}}</span>
+    </li>
+  </ol>
+  <h4 style="margin-top: 40px;">
+    {{new Date().toLocaleDateString()}}
+  </h4>
+  <ol>
+    <li v-for="user in $store.state.users" :key="user.id">
+      <NuxtLink :to="`list/${user.name}`">{{user.name}}</NuxtLink>
+      <span>- {{user.desc}}</span>
+    </li>
+  </ol>
 </div>
 </template>
-
-<script>
-export default {}
-</script>
