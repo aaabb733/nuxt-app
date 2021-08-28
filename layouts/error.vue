ce17548e4254f4799995e0730479891ff687b058
a <a>
2021-08-28 17:52:36 +0900

    a

---
+++ layouts/error.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        no page for {{$route.path}}
+    </div>
+</template>
--- store/index.js
+++ store/index.js
@@ -3,11 +3,16 @@ export const state = () => ({
         {
             id: 0,
             name: "ayumin",
-            desc: "Pokemon streamer"
+            desc: "Pokemon game streamer"
         },
         {
             id: 1,
             name: "katou junichi",
+            desc: "game streamer"
+        },
+        {
+            id: 2,
+            name: "mokou",
             desc: "game streamer"
         }
     ]
